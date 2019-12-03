e8c90df9fc0f1f99f401849556e25308a05c741e
Angus Williams <anguswilliams91@gmail.com>
2019-12-03 21:50:18 +0000

    add day one

---
+++ solutions/dayOne.hs
@@ -0,0 +1,18 @@
+import System.IO 
+import Control.Monad
+
+fuelNeeded :: Int -> Int
+fuelNeeded x = div x 3 - 2
+
+totalFuelNeeded :: Int -> Int
+totalFuelNeeded x
+    | fuelNeeded x <= 0 = 0
+    | otherwise = totalFuelNeeded (fuelNeeded x) + fuelNeeded x
+
+main = do
+    contents <- readFile "../inputs/dayOne.txt"
+    let mass = map read (lines contents) :: [Int]   
+    let partOne = sum (map fuelNeeded mass)
+    let partTwo = sum (map totalFuelNeeded mass)
+    print partOne
+    print partTwo
