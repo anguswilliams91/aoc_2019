aeb4b1e144cb40ff33ad7d219bba8b1f65acdb5c
Angus Williams <anguswilliams91@gmail.com>
2019-12-07 19:23:36 +0000

    finish day 3

--- solutions/dayThree.hs
+++ solutions/dayThree.hs
@@ -3,24 +3,36 @@ import Data.List
 import Data.List.Split
 import qualified Data.Set as Set
 
-step :: [(Int, Int)] -> String -> [(Int, Int)]
+step :: [(Int, (Int, Int))] -> String -> [(Int, (Int, Int))]
 step xs p
-    | d == 'R' = [(x + i, y) | i <- [1..s]] 
-    | d == 'L' = [(x - i, y) | i <- [1..s]]
-    | d == 'U' = [(x, y + i) | i <- [1..s]]
-    | d == 'D' = [(x, y - i) | i <- [1..s]]
+    | d == 'R' = [(j + i, (x + i, y)) | i <- [1..s]] 
+    | d == 'L' = [(j + i, (x - i, y)) | i <- [1..s]]
+    | d == 'U' = [(j + i, (x, y + i)) | i <- [1..s]]
+    | d == 'D' = [(j + i, (x, y - i)) | i <- [1..s]]
     where d = head p
           s = read (tail p) :: Int
-          (x, y) = last xs
+          (x, y) = snd (last xs)
+          j = fst (last xs)
 
-path :: [String] -> Set.Set (Int, Int)
-path xs = Set.fromList (tail (concat (scanl step [(0, 0)] xs)))
+path :: [String] -> Set.Set (Int, (Int, Int))
+path xs = Set.fromList (tail (concat (scanl step [(0, (0, 0))] xs)))
 
 manhattanDistance :: (Int, Int)  -> Int
 manhattanDistance (a, b) = abs a + abs b
 
-closestIntersection :: Set.Set (Int, Int) -> Set.Set (Int, Int) -> Int
-closestIntersection xs ys = minimum (Set.map manhattanDistance (Set.intersection xs ys))
+closestIntersection :: Set.Set (Int, (Int, Int)) -> Set.Set (Int, (Int, Int)) -> Int
+closestIntersection xs ys = minimum (Set.map manhattanDistance (Set.intersection xs1 ys1))
+    where xs1 = Set.map snd xs
+          ys1 = Set.map snd ys
+
+closestBySteps :: Set.Set (Int, (Int, Int)) -> Set.Set (Int, (Int, Int)) -> Int
+closestBySteps xs ys = head [fst x + fst y | x <- Set.toList xss, y <- Set.toList yss, snd x == snd y]
+    where zs = Set.intersection xs1 ys1
+          xs1 = Set.map snd xs
+          ys1 = Set.map snd ys
+          f x = Set.member (snd x) zs
+          xss = Set.filter f xs
+          yss = Set.filter f ys   
 
 main = do
     contents <- readFile "../inputs/dayThree.txt"
@@ -28,7 +40,9 @@ main = do
     let p1 = path (head directions)
     let p2 = path (last directions)
     let partOne = closestIntersection p1 p2
+    let partTwo = closestBySteps p1 p2
     print partOne
+    print partTwo
 
 
 
