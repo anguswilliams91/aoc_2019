4a40a4d5099b593e21c19bc3074ef16013ac1207
Angus Williams <anguswilliams91@gmail.com>
2019-12-14 11:51:51 +0000

    finish day 4

--- solutions/dayFour.hs
+++ solutions/dayFour.hs
@@ -1,5 +1,6 @@
 import System.IO
 import Data.Char
+import Data.List
 
 digits :: Integer -> [Integer]
 digits n = [toInteger (digitToInt x) | x <- show n]
@@ -9,15 +10,24 @@ neverDecrease n = and $ map (\x -> fst x >= snd x) xs
     where xs = zip (tail ys) (init ys)
           ys = digits n 
 
-
 hasDouble :: Integer -> Bool
 hasDouble n = or $ map (\x -> fst x == snd x) xs
     where xs = zip (tail ys) (init ys)
           ys = digits n
 
+count :: Eq a => a -> [a] -> Int
+count x = length . filter (x==)
+          
+atLeastOneDouble :: Integer -> Bool
+atLeastOneDouble n = elem 2 [count x xs | x <- nub xs]
+    where xs = digits n
 
-validPasswords :: Integer -> Integer -> [Integer]
-validPasswords x y = filter (\n -> neverDecrease n && hasDouble n) [x..y]
+partOne :: Integer -> Integer -> [Integer]
+partOne x y = filter (\n -> neverDecrease n && hasDouble n) [x..y]
+
+partTwo :: Integer -> Integer -> [Integer]
+partTwo x y = filter (\n -> neverDecrease n && hasDouble n && atLeastOneDouble n) [x..y]
 
 main = do
-    print . length $ validPasswords 153517 630395
+    print . length $ partOne 153517 630395
+    print . length $ partTwo 153517 630395
